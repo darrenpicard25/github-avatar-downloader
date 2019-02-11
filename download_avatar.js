6c5cad2bf0b3274b93a642f6da5ae51d8e30664c
Darren Picard <darrenpicard25@gmail.com>
2019-02-11 22:56:40 +0000

    Created file to hold token and added it to gitignore file

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,34 @@
 const request = require('request');
 
-console.log('hello');
+function getRepoContributors(repoOwner, repoName, cb) {
+  let options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+
+
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+
+
+
+
+
